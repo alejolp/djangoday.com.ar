c0490f2b32f73feb81ca4a70fdbe261842716b9b
Ubuntu <ubuntu@ip-10-28-127-52.ec2.internal>
2012-06-12 23:24:05 +0000

    alejandro

--- djangoday/deploy/wsgi.py
+++ djangoday/deploy/wsgi.py
@@ -11,7 +11,7 @@ import pinax.env
 #sys.path.append("/home/ubuntu/djangoday.com.ar/djangoday/apps")
 
 PROJECT_DIR = dirname(os.path.abspath(dirname(__file__)))
-APP_DIR = os.path.join(PROJECT_DIR, 'apps')
+APP_DIR = os.path.join(PROJECT_DIR,'apps')
 
 sys.path.append(APP_DIR)
 
@@ -20,3 +20,6 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'djangoday.settings'
 # setup the environment for Django and Pinax
 pinax.env.setup_environ(__file__)
 
+
+# set application for WSGI processing
+application = WSGIHandler()
